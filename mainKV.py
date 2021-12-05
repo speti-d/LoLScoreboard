8f2a016992bb4e04a0883bf0fdd56895e47516d2
BencePe <94329462+BencePe@users.noreply.github.com>
2021-12-05 19:42:40 +0100

    Add files via upload

--- mainKV.py
+++ mainKV.py
@@ -1,4 +1,6 @@
 import kivy
+import json
+import os
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -14,7 +16,12 @@ import SummonerDTO
 
 class MyGridLayout(GridLayout):
     def __init__(self):
+        self.cwd = os.getcwd()
         super().__init__()
+        f = open('champion.json')
+        championData = json.load(f)
+        f.close()
+        championNames = list(championData['data'])
         self.summonerName = 'IntOrString'
         self.APIKey = 'RGAPI-894bac97-4a8c-4ef9-91e6-2a59e0539551'
         response = SummonerDTO.getSummonerData(self.summonerName, self.APIKey)
@@ -28,43 +35,52 @@ class MyGridLayout(GridLayout):
         for i in range(3):
             self.championId[i] = response[i]['championId']
             self.championPoints[i] = response[i]['championPoints']
+            for j in range(len(championNames)):
+                if str(championData['data'][championNames[j]]['key']) == str(self.championId[i]):
+                    self.championId[i] = championNames[j]
+
         self.championPortrait = [0, 0, 0, 0, 0]
         self.kills = [0, 0, 0, 0, 0]
         self.assists = [0, 0, 0, 0, 0]
         self.deaths = [0, 0, 0, 0, 0]
         self.cs = [0, 0, 0, 0, 0]
+        self.win = [0, 0, 0, 0, 0]
         self.matchIds = MatchList.getMatchList(self.puuid, self.APIKey)
         for i in range(5):
             response = LastMatch.getLastMatch(self.matchIds[i], self.APIKey)
             for j in range(len(response['info']['participants'])):
                 if response['info']['participants'][j]['summonerName'] == self.summonerName:
-                    self.championPortrait[i] = response['info']['participants'][j]['championId']
+                    self.championPortrait[i] = response['info']['participants'][j]['championName']
                     self.kills[i] = response['info']['participants'][j]['kills']
                     self.assists[i] = response['info']['participants'][j]['assists']
                     self.deaths[i] = response['info']['participants'][j]['deaths']
                     self.cs[i] = response['info']['participants'][j]['totalMinionsKilled']
-
+                    self.win[i] = response['info']['participants'][j]['win']
+                    if self.win[i] == True:
+                        self.win[i] = '           Win'
+                    else:
+                        self.win[i] = '           Lose'
 
     def update(self):
-        self.ids.summ_icon.text = str(self.profileIconId)
+        self.ids.summ_icon.source = source = str(self.cwd) + '\profileicon\\'+str(self.profileIconId) + '.png'
         self.ids.summ_name.text = str(self.summonerName)
-        self.ids.summ_level.text = str(self.level)
-        self.ids.champ_mastery1P.text = str(self.championId[0])
+        self.ids.summ_level.text = 'Level ' + str(self.level)
+        self.ids.champ_mastery1P.source = str(self.cwd) + '\champion\\'+str(self.championId[0]) + '.png'
         self.ids.champ_mastery1.text = str(self.championPoints[0])
-        self.ids.champ_mastery2P.text = str(self.championId[1])
+        self.ids.champ_mastery2P.source = str(self.cwd) + '\champion\\'+str(self.championId[1]) + '.png'
         self.ids.champ_mastery2.text = str(self.championPoints[1])
-        self.ids.champ_mastery3P.text = str(self.championId[2])
+        self.ids.champ_mastery3P.source = str(self.cwd) + '\champion\\'+str(self.championId[2]) + '.png'
         self.ids.champ_mastery3.text = str(self.championPoints[2])
-        self.ids.match1p.text = str(self.championPortrait[0])
-        self.ids.match1kda.text = str(self.kills[0]) + str(self.deaths[0]) + str(self.assists[0]) + str(self.cs[0])
-        self.ids.match2p.text = str(self.championPortrait[1])
-        self.ids.match2kda.text = str(self.kills[1]) + str(self.deaths[1]) + str(self.assists[1]) + str(self.cs[1])
-        self.ids.match3p.text = str(self.championPortrait[2])
-        self.ids.match3kda.text = str(self.kills[2]) + str(self.deaths[2]) + str(self.assists[2]) + str(self.cs[2])
-        self.ids.match4p.text = str(self.championPortrait[3])
-        self.ids.match4kda.text = str(self.kills[3]) + str(self.deaths[3]) + str(self.assists[3]) + str(self.cs[3])
-        self.ids.match5p.text = str(self.championPortrait[4])
-        self.ids.match5kda.text = str(self.kills[4]) + str(self.deaths[4]) + str(self.assists[4]) + str(self.cs[4])
+        self.ids.match0p.source = str(self.cwd) + '\champion\\' + str(self.championPortrait[0]) + '.png'
+        self.ids.match0kda.text = str(self.kills[0]) +"/"+ str(self.deaths[0])+"/"+str(self.assists[0]) + " - "+str(self.cs[0]) + " cs " + str(self.win[0])
+        self.ids.match1p.source = str(self.cwd) + '\champion\\' + str(self.championPortrait[1]) + '.png'
+        self.ids.match1kda.text = str(self.kills[1])+"/"+str(self.deaths[1])+"/"+str(self.assists[1]) + " - "+str(self.cs[1]) + " cs " + str(self.win[1])
+        self.ids.match2p.source = str(self.cwd) + '\champion\\' + str(self.championPortrait[2]) + '.png'
+        self.ids.match2kda.text = str(self.kills[2])+"/"+str(self.deaths[2])+"/"+str(self.assists[2]) + " - "+str(self.cs[2]) + " cs " + str(self.win[2])
+        self.ids.match3p.source = str(self.cwd) + '\champion\\' + str(self.championPortrait[3]) + '.png'
+        self.ids.match3kda.text = str(self.kills[3])+"/"+str(self.deaths[3])+"/"+str(self.assists[3]) + " - "+str(self.cs[3]) + " cs " + str(self.win[3])
+        self.ids.match4p.source = str(self.cwd) + '\champion\\' + str(self.championPortrait[4]) + '.png'
+        self.ids.match4kda.text = str(self.kills[4])+"/"+str(self.deaths[4])+"/"+str(self.assists[4]) + " - "+str(self.cs[4]) + " cs " + str(self.win[4])
 
 class MyApp(App):
     def build(self):
